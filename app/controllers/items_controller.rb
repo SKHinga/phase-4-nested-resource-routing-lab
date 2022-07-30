a0f9e658a753f8bfc5a8ade5afb842836f2bf578
Samuel Hinga <samuelhinga200@gmail.com>
2022-07-30 05:39:55 +0300

    commit tree 53d930490d08265c05e3207528f53796f1c992bd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    return render json: items, include: :user
+  end
+
+  def show
+    items =   Item.find(params[:id])
+    render json: items
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    items = user.items.create(item_params)
+    render json: items, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Review not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
